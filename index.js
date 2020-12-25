4a204c284b2a715ae3df97ec7e20d66f2b0c9779
Oskar Andersson <58656584+oskand01@users.noreply.github.com>
2020-12-26 00:42:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-let date = new Date();
+function Time() {
+  this.date = new Date();
+  this.hour = this.date.getHours();
+  this.minute = this.date.getMinutes();
+  this.second = this.date.getSeconds();
+}
 
-let hour = date.getHours();
-let minutes = date.getMinutes();
-let seconds = date.getSeconds();
+let time = new Time();
 
-console.log(hour);
-console.log(minutes);
-console.log(seconds);
+let hours = document.getElementById("hour");
+let minutes = document.getElementById("minute");
+let seconds = document.getElementById("second")
 
+hours.textContent = time.hour;
+minutes.textContent = time.minute;
+seconds.textContent = time.second;
+
+function setHour () {
+
+}
