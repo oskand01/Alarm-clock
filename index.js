f827b04ed197c6ac643f7e57fdd2687cee00425b
Oskar Andersson <oskand01@gmail.com>
2020-12-25 22:15:27 +0100

    created files

---
+++ index.js
@@ -0,0 +1,10 @@
+let date = new Date();
+
+let hour = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+
+console.log(hour);
+console.log(minutes);
+console.log(seconds);
+
