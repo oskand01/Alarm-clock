b68cc137b3070bc3ef72f9a8258006534d693683
Oskar Andersson <oskand01@gmail.com>
2020-12-30 09:52:58 +0100

    bit of everything

--- index.js
+++ index.js
@@ -1,71 +1,67 @@
 "use strict";
 
-function Time() {
-  this.time = new Date();
-  this.hour = this.time.getHours();
-  this.minute = this.time.getMinutes();
-  this.second = this.time.getSeconds();
-
-  this.year = this.time.getFullYear();
-  this.month = this.time.getMonth() + 1;
-  this.day = this.time.getDate();
-}
+const hoursSpan = document.getElementById("hour");
+const minutesSpan = document.getElementById("minute");
+const secondsSpan = document.getElementById("second");
+const dateParagraf = document.getElementById("date");
 
 function setDate() {
-  const date = new Time();
-  const dateParagraf = document.getElementById("date");
-  let month = date.month;
+  const date = new Date();
+
+  let month = date.getMonth() + 1;
   if (month < 10) {
     month = `0${month}`;
   }
-  let day = date.day;
+
+  let day = date.getDate();
   if (day < 10) {
     day = `0${day}`;
   }
-  dateParagraf.textContent = `${date.year}-${month}-${day}`;
+  dateParagraf.textContent = `${date.getFullYear()}-${month}-${day}`;
 }
 
 function setTime() {
-  const time = new Time();
-  const hours = document.getElementById("hour");
-  const minutes = document.getElementById("minute");
-  const seconds = document.getElementById("second");
+  const time = new Date();
+  const hour = time.getHours();
+  const minute = time.getMinutes();
+  const second = time.getSeconds();
 
-  hours.textContent = `${time.hour}:`;
-  if (time.hour < 10) {
-    hours.textContent = `0${time.hour}:`;
-  }
-  minutes.textContent = `${time.minute}:`;
-  if (time.minute < 10) {
-    minutes.textContent = `0${time.minute}:`;
+  hoursSpan.textContent = `${hour}:`;
+  if (hour < 10) {
+    hoursSpan.textContent = `0${hour}:`;
   }
 
-  seconds.textContent = `${time.second}`;
-  if (time.second < 10) {
-    seconds.textContent = `0${time.second}`;
+  minutesSpan.textContent = `${minute}:`;
+  if (minute < 10) {
+    minutesSpan.textContent = `0${minute}:`;
   }
 
-  if (time.hour === 0 && time.minute === 0 && time.second === 0) {
+  secondsSpan.textContent = `${second}`;
+  if (second < 10) {
+    secondsSpan.textContent = `0${second}`;
+  }
+
+  if (hour === 0 && minute === 0 && second === 0) {
     setDate();
-    
+  } else if (minute === 59 && second > 50) {
+    transform();
   }
-  console.log(time.second)
- transform();
 }
 
-function transform (){
+
+//last ten seconds every hour the
+function transform() {
   let element = document.getElementById("container");
   element.style.transition = "all 0.1s linear";
   element.style.transform = "scale(1.0025)";
   element.style.backgroundColor = "#f8cbc9";
-  element.style.top = "5.3vw"
+  element.style.top = "5.1vw";
 
   setTimeout(() => {
     element.style.transform = "scale(1)";
     element.style.backgroundColor = "#f4afab";
-    element.style.top = "5vw"
+    element.style.top = "5vw";
   }, 500);
-  
 }
 
 setDate();
