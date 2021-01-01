9707ec7319abb7e4de0a3ec7f0c3fbad8752057c
Oskar Andersson <oskand01@gmail.com>
2021-01-01 01:15:49 +0100

    happy new year

--- index.js
+++ index.js
@@ -56,15 +56,20 @@ function initiateNewAlarmButton() {
   newAlarmButton.addEventListener("click", () => {
     alarmContainer.style.display = "block";
     clockContainer.style.display = "none";
+    newAlarmButton.style.display = "none";
     initiateExitAlarmButton();
   });
 }
 
 function initiateExitAlarmButton() {
   const exitAlarmButton = document.getElementById("exit-alarm-button");
+  exitAlarmButton.style.display = "inline-block";
+  
   exitAlarmButton.addEventListener("click", () => {
     clockContainer.style.display = "block";
     alarmContainer.style.display = "none";
+    exitAlarmButton.style.display = "none";
+    newAlarmButton.style.display = "inline-block";
   });
 }
 
@@ -90,8 +95,8 @@ function setTime() {
   }
 
   if (hour === 0 && minute === 0 && second === 0) {
-    getDate();
-  } else if (second > 49) {
+    dateParagraf.textContent = getDate();
+  } else if (hour === 23 && minute === 59 && second > 49) {
     transform();
   }
 }
